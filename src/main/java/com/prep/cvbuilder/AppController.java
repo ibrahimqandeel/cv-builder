35bbcd6059438ffcf06681ab3c9a18656693c8d6
ikandeel <ibrahim.kandeel@zenjob.com>
2022-10-02 19:12:34 +0200

    - application enhancements

--- src/main/java/com/prep/cvbuilder/AppController.java
+++ src/main/java/com/prep/cvbuilder/AppController.java
@@ -1,15 +1,20 @@
 package com.prep.cvbuilder;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/health")
+@RequestMapping("api/app-name")
 public class AppController {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @GetMapping
-    public String checkApp(){
-        return "Hello App";
+    public String getAppName(){
+        return "Hello from -> " + applicationName;
     }
 }
